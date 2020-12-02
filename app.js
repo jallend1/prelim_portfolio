2ba62009deebfca11f70ca11c75edf55c57aa572
jallend1 <jallend1@gmail.com>
2020-12-01 18:10:52 -0800

    Implements basic scrollIntoView Javascript instead of exclusively CSS

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const panels = document.getElementsByClassName('panel');
+let activeIndex = 0;
+
+handleKey = key => {
+  if(key === 'ArrowDown'){
+    activeIndex < panels.length - 1 ? activeIndex++ : activeIndex = 0;
+  }
+  if(key === 'ArrowUp'){
+    activeIndex !== 0 ? activeIndex-- : activeIndex = panels.length - 1;
+  }
+  panels[activeIndex].scrollIntoView({behavior: 'smooth'});
+}
+
+document.addEventListener('keydown', (e) => handleKey(e.code));
+panels[activeIndex].scrollIntoView();
+
+
 console.log(`
 ┏┓   In this 
 ┃┃╱╲ house we 
