bfcf833d55afb27c904f389f2d6c0bee5a0f93fc
jallend1 <jallend1@gmail.com>
2020-12-03 16:53:20 -0800

    Begins implementation of scroll wheel utilization for navigation

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const panels = document.getElementsByClassName('panel');
 let activeIndex = 0;
+let previousScrollTime = null;
 
-handleKey = key => {
+const handleKey = key => {
   if(key === 'ArrowDown'){
     activeIndex < panels.length - 1 ? activeIndex++ : activeIndex = 0;
   }
   if(key === 'ArrowUp'){
     activeIndex !== 0 ? activeIndex-- : activeIndex = panels.length - 1;
   }
+  jumpToSpot();
+}
+
+const jumpToSpot = () => {
   panels[activeIndex].scrollIntoView({behavior: 'smooth'});
 }
 
+const mouseWheelScroll = (e) => {
+  if(!previousScrollTime || Date.now() - previousScrollTime > 200){
+    if(e.deltaY > 0){
+      console.log('Going down!')
+      activeIndex < panels.length - 1 ? activeIndex++ : activeIndex = 0;
+    }
+    else if(e.deltaY < 0){
+      console.log('Going up!')
+      activeIndex !== 0 ? activeIndex-- : activeIndex = panels.length - 1;
+    }
+    previousScrollTime = Date.now();
+  }
+  jumpToSpot();
+}
+
 document.addEventListener('keydown', (e) => handleKey(e.code));
-panels[0].scrollIntoView();
+window.addEventListener('wheel', mouseWheelScroll)
+
+jumpToSpot();
 
 
 console.log(`
