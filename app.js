7feab389421dc8d83535f46c9a4c6975f3988f37
jallend1 <jallend1@gmail.com>
2020-12-03 17:38:11 -0800

    Adds ability to click through navigation menu

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const panels = document.getElementsByClassName('panel');
+const panels = document.querySelectorAll('.panel');
+const nav = document.querySelector('#nav-ul');
+
 let activeIndex = 0;
 let previousScrollTime = null;
 
@@ -16,14 +18,14 @@ const jumpToSpot = () => {
   panels[activeIndex].scrollIntoView({behavior: 'smooth'});
 }
 
+// This function is _not_ lining up properly at this time
+// handleKey and MouseWheelScroll should be integrated once the flaw in scrolling is resolved
 const mouseWheelScroll = (e) => {
   if(!previousScrollTime || Date.now() - previousScrollTime > 200){
     if(e.deltaY > 0){
-      console.log('Going down!')
       activeIndex < panels.length - 1 ? activeIndex++ : activeIndex = 0;
     }
     else if(e.deltaY < 0){
-      console.log('Going up!')
       activeIndex !== 0 ? activeIndex-- : activeIndex = panels.length - 1;
     }
     previousScrollTime = Date.now();
@@ -31,8 +33,21 @@ const mouseWheelScroll = (e) => {
   jumpToSpot();
 }
 
+const navClick = (e) => {
+  // Extracts text from clicked nav element
+  const destinationID = e.target.textContent.toLowerCase();
+  panels.forEach((panel, index) => {
+    if(panel.id === destinationID){
+      activeIndex = index;
+    }
+  })
+  jumpToSpot();
+}
+
 document.addEventListener('keydown', (e) => handleKey(e.code));
-window.addEventListener('wheel', mouseWheelScroll)
+window.addEventListener('wheel', mouseWheelScroll);
+nav.addEventListener('click', navClick);
+
 
 jumpToSpot();
 
